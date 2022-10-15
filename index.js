f0b36283d5638df3afebe40426c6a44af2b89f4a
neila-a <neilaspace@outlook.com>
2022-10-15 14:35:32 +0800

    更改为<h4>

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const fs = require('fs');
 const moment = require('moment');
-let addText = `Today is ${moment().format("GGGG - MM - DD")}.`;
+let addText = moment().format("GGGG - MM - DD");
 fs.writeFileSync(
     './README.md', 
     fs.readFileSync('./README.md')
     .toString()
-    .replace(/Today is [0-9][0-9][0-9][0-9] - [0-9][0-9] - [0-9][0-9]./gs, addText));
+    .replace(/[0-9][0-9][0-9][0-9] - [0-9][0-9] - [0-9][0-9]/gs, addText));

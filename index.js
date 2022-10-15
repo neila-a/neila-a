1caf2e7f2d2f992fce4ad0e43589bc5509090f3e
neila-a <neilaspace@outlook.com>
2022-10-15 14:27:23 +0800

    可能正常了吧?

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
-const fs = require('fs')
-const moment = require('moment')
-let addText = 
-let fileText = fs.readFileSync('./README.md').toString().replace(/Today is [0-9][0-9][0-9][0-9] - [0-9][0-9] - [0-9][0-9]/gs, "")
-let text = fileText + addText
-fs.writeFileSync('./README.md', text)
+const fs = require('fs');
+const moment = require('moment');
+let addText = `Today is ${moment().format("GGGG - MM - DD")}.`;
+fs.writeFileSync(
+    './README.md', 
+    fs.readFileSync('./README.md')
+    .toString()
+    .replace(/Today is [0-9][0-9][0-9][0-9] - [0-9][0-9] - [0-9][0-9]./gs, addText));

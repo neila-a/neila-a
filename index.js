134eea94a47a637fbbb5864a263050d61357dcb6
neila <78797625+neila-a@users.noreply.github.com>
2022-10-15 13:27:19 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
 const fs = require('fs')
 let fileText = fs.readFileSync('./README.md').toString()
-
-let editedFileText = `${fileText}Today is a nice day!`
-
-fs.writeFileSync('./README.md', editedFileText)
+let addText = fs.readFileSync('./addText.md').toString() 
+fs.writeFileSync('./README.md', fileText + addText)

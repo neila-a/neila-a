16d2b3e229c75ddb5a9c457e25421064785772a8
neila <78797625+neila-a@users.noreply.github.com>
2022-10-16 15:32:44 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const fs = require('fs');
 const moment = require('moment');
-let addText = moment().format("GGGG - MM - DD");
+let addText = moment().format("GGGG - MM - DD, HH:mm");
 fs.writeFileSync(
     './README.md', 
     fs.readFileSync('./README.md')
     .toString()
-    .replace(/[0-9][0-9][0-9][0-9] - [0-9][0-9] - [0-9][0-9]/gs, addText));
+    .replace(/[0-9][0-9][0-9][0-9] - [0-9][0-9] - [0-9][0-9], [0-9][0-9]:[0-9][0-9]/gs, addText));

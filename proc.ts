9c9af1f58942da1225beadcab110d73486c892da
Neila <neilaspace@outlook.com>
2023-01-31 11:12:55 +0800

    Update proc.ts

--- proc.ts
+++ proc.ts
@@ -1,7 +1,6 @@
-import * as moment from "moment";
+
 import * as fs from "fs";
 import * as ejs from "ejs";
-fs.writeFileSync('./README.md', ejs.render(fs.readFileSync('./README.ejs').toString(), {
-    date: moment().format("GGGG - MM - DD")
-}));
-console.log(`[${moment().format("HH:mm:ss")}] README process done.`);
+fs.writeFileSync('./README.md', ejs.render(fs.readFileSync('./README.ejs').toString()
+));
+

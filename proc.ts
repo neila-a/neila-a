01a352643bce993757039ff125a9cd33f85c79de
Neila <neilaspace@outlook.com>
2023-01-31 11:15:31 +0800

    修改import

--- proc.ts
+++ proc.ts
@@ -1,6 +1,3 @@
-
 import * as fs from "fs";
 import * as ejs from "ejs";
-fs.writeFileSync('./README.md', ejs.render(fs.readFileSync('./README.ejs').toString()
-));
-
+fs.writeFileSync('./README.md', ejs.render(fs.readFileSync('./README.ejs').toString()));
